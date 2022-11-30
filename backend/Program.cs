8434af6dc406994fb7dd4612dc47b22d420adc34
johndward01 <johndward01@gmail.com>
2022-11-30 14:48:19 -0600

    added route handling for frontend project

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSpaStaticFiles(configuration: options => { options.RootPath = "wwwroot"; });
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("VueCorsPolicy", builder =>
+    {
+        builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins("https://localhost:5001");
+    });
+});
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
